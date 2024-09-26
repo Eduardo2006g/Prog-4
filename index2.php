3a0f8c39be64b9c4a2db9c785c1170ebc4f69b64
eduardo2006g <duduoliveirae11@gmail.com>
2024-09-26 13:02:43 -0300

    atv 2 pronta

---
+++ index2.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+
+echo '<h1>URLs Absolutas</h1>';
+
+echo '<a href="https://www.w3schools.com/">W3C</a>';
+echo '<a href="https://www.google.com.br/?hl=pt-BR">Google</a>';
+
+echo '<h1>URLs relativas</h1>';
+
+echo '<a href="https://www.w3schools.com/html/default.asp">HTML Images</a>';
+echo '<a href="https://www.w3schools.com/css/default.asp">CSS Tutorial</a>';
+
+?>
+</body>
+</html>
