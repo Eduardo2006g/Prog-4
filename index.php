c26489e41420fdeca0f498c0a2bf9d19506dbd9f
eduardo2006g <duduoliveirae11@gmail.com>
2024-09-26 12:46:42 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php 
+
+echo '<img src="https://picsum.photos/500/600"></img>';
+
+echo '<h1>Imagens da semana 2</h1>'
+
+?>
+</body>
+
+</body>
+</html>
